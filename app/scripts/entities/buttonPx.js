ae532b6a8d8e8c0c1c478c0cb4601dc130b91a9c
Justin McCandless <justinjmccandless@gmail.com>
2014-05-12 22:17:30 -0700

    cool new buttons with a really funny bug

--- app/scripts/entities/buttonPx.js
+++ app/scripts/entities/buttonPx.js
@@ -16,7 +16,14 @@ define(['jquery'], function ($) {
         ButtonPx.prototype.fillStyle = 'rgb(255, 255, 255)';
         ButtonPx.prototype.fillStyleButton = 'rgb(91, 131, 78)';
         ButtonPx.prototype.strokeStyle = 'rgb(255, 255, 255)';
-        ButtonPx.prototype.qOffset = 16;
+        ButtonPx.prototype.qOffset = 4;
+        ButtonPx.prototype.dOffset = 14;
+        ButtonPx.prototype.pressOffset = 6;
+        ButtonPx.prototype.clickDuration = 1;
+
+        ButtonPx.prototype.clickedAgo = 0;
+        ButtonPx.prototype.clickedStage = 0;
+        ButtonPx.prototype.event = null;
 
         function ButtonPx(x, y, text, callback, width, height, fillStyle) {
             if (!width) {
@@ -33,17 +40,38 @@ define(['jquery'], function ($) {
         }
 
         ButtonPx.prototype.render = function(ctx, dt) {
-            // Draw the rectangle
-            ctx.strokeStyle = this.strokeStyle;
-            ctx.beginPath();
-            ctx.moveTo(this.x, this.y);
-            ctx.quadraticCurveTo(this.x + this.width, this.y - this.qOffset, this.x + this.width, this.y);
-            ctx.quadraticCurveTo(this.x + this.width + this.qOffset, this.y + this.height, this.x + this.width, this.y + this.height);
-            ctx.quadraticCurveTo(this.x + this.width / 2, this.y + this.height + this.qOffset, this.x, this.y + this.height);
-            ctx.quadraticCurveTo(this.x - this.qOffset, this.y + this.height / 4, this.x - 4, this.y - 10);
-            ctx.stroke();
-            ctx.fillStyle = this.fillStyleButton;
-            ctx.fill();
+            // Just clicked, press
+            if (this.clickedStage === 1) {
+                this.clickedAgo += dt;
+                this.y += this.pressOffset;
+                this.dOffset -= this.pressOffset;
+                this.clickedStage = 2;
+            }
+            // Keep pressed for half of duration
+            else if (this.clickedStage === 2) {
+                this.clickedAgo += dt;
+
+                if (this.clickedAgo >= this.clickDuration / 2) {
+                    this.clickedStage = 3;
+                }
+            }
+            // Depress
+            else if (this.clickedStage === 3) {
+                this.clickedAgo += dt;
+                this.y -= this.pressOffset;
+                this.dOffset += this.pressOffset;
+
+                if (this.clickedAgo >= this.clickDuration) {
+                    this.clickedStage = 4;
+                }
+            }
+            // Call callback and be done
+            else if (this.clickedStage === 4) {
+                this.callback(this.event);
+            }
+
+            // Draw the button
+            this.drawButton(ctx);
 
             // Draw the text
             if (this.font !== null) {
@@ -53,10 +81,38 @@ define(['jquery'], function ($) {
             ctx.fillText(this.text, this.x + this.padding, this.y + 2 * this.height / 3, this.width - this.padding * 2);
         };
 
+        ButtonPx.prototype.drawButton = function(ctx) {
+            // Draw the rectangle
+            ctx.strokeStyle = this.strokeStyle;
+            ctx.beginPath();
+            ctx.moveTo(this.x, this.y);
+            ctx.quadraticCurveTo(this.x + this.width / 2, this.y - this.qOffset, this.x + this.width, this.y);
+            ctx.quadraticCurveTo(this.x + this.width + this.qOffset, this.y + this.height / 2, this.x + this.width, this.y + this.height);
+            ctx.quadraticCurveTo(this.x + this.width / 2, this.y + this.height + this.qOffset, this.x, this.y + this.height);
+            ctx.quadraticCurveTo(this.x - this.qOffset, this.y + this.height / 2, this.x, this.y);
+            ctx.closePath();
+            ctx.lineWidth = 5;
+            ctx.stroke();
+            ctx.fillStyle = this.fillStyleButton;
+            ctx.fill();
+
+            // Draw the shadow
+            ctx.strokeStyle = this.strokeStyle;
+            ctx.beginPath();
+            ctx.moveTo(this.x + this.width, this.y);
+            ctx.lineTo(this.x + this.width + this.dOffset, this.y + this.dOffset);
+            ctx.quadraticCurveTo(this.x + this.width + 2 * this.dOffset / 3, this.y + this.height + this.dOffset / 2, this.x + this.width + this.dOffset, this.y + this.height + this.dOffset);
+            ctx.quadraticCurveTo(this.x + this.dOffset / 2, this.y + this.height + this.dOffset / 2, this.x + this.dOffset, this.y + this.height + this.dOffset);
+            ctx.lineTo(this.x, this.y + this.height);
+            ctx.lineWidth = 3;
+            ctx.stroke();
+        };
+
         // ButtonPx click event, has to check if click was inside the button!
         ButtonPx.prototype.click = function(event, scene) {
             if (hoopty.scenes.Scene.isInside(scene.getEventCoords(event), this) && this.display) {
-                this.callback(event);
+                this.event = event;
+                this.clickedStage = 1;
             }
         };
 
